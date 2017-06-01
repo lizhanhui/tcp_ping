900c20e18845d469cf1a254e2998b38b628fb081
Li Zhanhui <lizhanhui@ali-186590d602cd.local>
2017-06-01 16:15:07 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by Li Zhanhui on 2017/6/1.
+//  Copyright © 2017年 Li Zhanhui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
